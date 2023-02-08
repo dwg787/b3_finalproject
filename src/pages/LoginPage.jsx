6f2bcc6fae99c759469e79d3be357bed570a5731
(yooyoungjae) <(dbdudwo0910@gmail.com)>
2023-02-08 10:19:31 +0900

    re commit login

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Kakao from "../Login/Kakao";
+import Naver from "../Login/Naver";
+import LoginTest from "../LoginTest/LoginTest";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Kakao />
+      <Naver />
+      {/* <LoginTest /> */}
+    </>
+  );
+}
