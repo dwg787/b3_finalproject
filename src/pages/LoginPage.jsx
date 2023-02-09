0d9f3542bddc6714d6cfe0eab3cf7c997e65a595
daeho sim <tlaeogh5@gmail.com>
2023-02-09 10:55:59 +0900

    Edit:ㅎㅇ

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,61 +1,50 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { auth, provider } from '../apis/firebase';
-import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
-import KakaoLoginButton from '../components/Login/KakaoLoginButton';
-import KakaoLogoutButton from '../components/Login/KakaoLogoutButton';
-import Naver from '../components/Login/Naver';
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from "../apis/firebase";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import KakaoLoginButton from "../components/Login/KakaoLoginButton";
+import KakaoLogoutButton from "../components/Login/KakaoLogoutButton";
+import Naver from "../components/Login/Naver";
 
 const LoginPage = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const navigate = useNavigate();
 
+  // 구글로그인
   const handleclick = () => {
     signInWithPopup(auth, provider).then((data) => {
       setValue(data.user.email);
-      sessionStorage.setItem('email', data.user.email);
-      navigate('/');
+      sessionStorage.setItem("email", data.user.email);
+      navigate("/");
     });
   };
 
   const logIn = async () => {
-    const login = await signInWithEmailAndPassword(
-      auth,
-      emailRef.current.value,
-      passwordRef.current.value
-    );
-    alert('login 성공!');
+    const login = await signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value);
+    alert("login 성공!");
     console.log(login);
-    console.log('이메일', emailRef.current.value);
-    console.log('비번', passwordRef.current.value);
-    sessionStorage.setItem('email', login.user.email);
-    navigate('/');
+    console.log("이메일", emailRef.current.value);
+    console.log("비번", passwordRef.current.value);
+    sessionStorage.setItem("email", login.user.email);
+    navigate("/");
   };
 
   useEffect(() => {
-    setValue(sessionStorage.getItem('email'));
+    setValue(sessionStorage.getItem("email"));
   });
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <div>로그인 페이지</div>
       <div>
-        <input
-          ref={emailRef}
-          type='text'
-          placeholder='이메일을 입력해주세요.'
-        />
+        <input ref={emailRef} type="text" placeholder="이메일을 입력해주세요." />
       </div>
       <div>
-        <input
-          ref={passwordRef}
-          type='password'
-          placeholder='비밀번호를 입력해주세요'
-        />
+        <input ref={passwordRef} type="password" placeholder="비밀번호를 입력해주세요" />
       </div>
-      <button onClick={logIn} type='submit'>
+      <button onClick={logIn} type="submit">
         login
       </button>
       <button onClick={handleclick}>google로그인</button>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,36 +1,33 @@
-import React from 'react';
-import { auth, db } from '../apis/firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { collection, addDoc } from 'firebase/firestore';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { auth, db } from "../apis/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, addDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
   const navigate = useNavigate();
-  const id_ref = React.useRef('');
-  const name_ref = React.useRef('');
-  const pw_ref = React.useRef('');
+  const id_ref = React.useRef("");
+  const name_ref = React.useRef("");
+  const pw_ref = React.useRef("");
   const signup = async () => {
-    if (id_ref.current.value === '') {
+    if (id_ref.current.value === "") {
       return false;
     }
-    const user = await createUserWithEmailAndPassword(
-      auth,
-      id_ref.current.value,
-      pw_ref.current.value
-    );
+    const user = await createUserWithEmailAndPassword(auth, id_ref.current.value, pw_ref.current.value);
+    sessionStorage.setItem("email", user.user.email);
     console.log(user);
-    const user_doc = await addDoc(collection(db, 'users'), {
+    const user_doc = await addDoc(collection(db, "users"), {
       user_id: user.user.email,
       name: name_ref.current?.value,
     });
-    navigate('/');
+    navigate("/");
     console.log(user_doc.id);
   };
   return (
     <div>
       이메일: <input ref={id_ref} /> <br />
       이름 : <input ref={name_ref} /> <br />
-      비밀번호 : <input ref={pw_ref} type='password' /> <br />
+      비밀번호 : <input ref={pw_ref} type="password" /> <br />
       <button onClick={signup}>회원가입</button>
     </div>
   );
