7c80d2ec1d8ad7b08c4b21089b003557118bf372
(yooyoungjae) <(dbdudwo0910@gmail.com)>
2023-02-14 12:21:23 +0900

    conflict

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader/Loader";
 import Navbar from "./components/Navbar";
 import ChatBotModal from "./components/bot/ChatBotModal";
 import TopButton from "./components/TopButton/TopButton";
-=======
-import { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Loader from './components/Loader/Loader';
-import Navbar from './components/Navbar';
-import ChatBotModal from './components/bot/ChatBotModal';
-import TopButton from './components/TopButton/TopButton';
-import Ticketing from './components/Reservation/Ticketing';
->>>>>>> 402ac7723ccd2ebb9db2de63fb9623212a9ca2d3
+import Ticketing from "./components/Reservation/Ticketing";
+
 import {
   MainPage,
   LoginPage,
@@ -26,11 +18,7 @@ import {
   SearchPage,
   StayDetailPage,
   RestaurantDetailPage,
-<<<<<<< HEAD
 } from "./pages";
-=======
-} from './pages';
->>>>>>> 402ac7723ccd2ebb9db2de63fb9623212a9ca2d3
 
 const Router = () => {
   return (
@@ -40,7 +28,6 @@ const Router = () => {
         <ChatBotModal />
         <TopButton />
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/my" element={<MyPage />} />
@@ -52,19 +39,6 @@ const Router = () => {
           <Route path="/purchase" element={<PurchasePage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/search" element={<SearchPage />} />
-=======
-          <Route path='/' element={<MainPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/my' element={<MyPage />} />
-          <Route path='/spot/:id' element={<DetailPage />} />
-          <Route path='/spot/:id/map' element={<MapPage />} />
-          <Route path='/stay/:id' element={<StayDetailPage />} />
-          <Route path='/restaurant/:id' element={<RestaurantDetailPage />} />
-          <Route path='/stay/:id/reservation' element={<ReservationPage />} />
-          <Route path='/purchase' element={<PurchasePage />} />
-          <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/search' element={<SearchPage />} />
->>>>>>> 402ac7723ccd2ebb9db2de63fb9623212a9ca2d3
         </Routes>
       </Suspense>
     </BrowserRouter>
