131dc28552663e5f57913c413c6ba46854db0b3b
daeho sim <tlaeogh5@gmail.com>
2023-02-14 09:27:40 +0900

    Edit: 내정보수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,23 +1,11 @@
-import { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Loader from './components/Loader/Loader';
-import Navbar from './components/Navbar';
-import ChatBotModal from './components/bot/ChatBotModal';
-import TopButton from './components/TopButton/TopButton';
-import Ticketing from './components/Reservation/Ticketing';
-import {
-  MainPage,
-  LoginPage,
-  ReservationPage,
-  MyPage,
-  DetailPage,
-  MapPage,
-  PurchasePage,
-  SignUpPage,
-  SearchPage,
-  StayDetailPage,
-  RestaurantDetailPage,
-} from './pages';
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loader from "./components/Loader/Loader";
+import Navbar from "./components/Navbar";
+import ChatBotModal from "./components/bot/ChatBotModal";
+import TopButton from "./components/TopButton/TopButton";
+import Ticketing from "./components/Reservation/Ticketing";
+import { MainPage, LoginPage, ReservationPage, MyPage, DetailPage, MapPage, PurchasePage, SignUpPage, SearchPage, StayDetailPage, RestaurantDetailPage } from "./pages";
 
 const Router = () => {
   return (
@@ -27,7 +15,6 @@ const Router = () => {
         <ChatBotModal />
         <TopButton />
         <Routes>
-
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/my" element={<MyPage />} />
@@ -40,9 +27,6 @@ const Router = () => {
           <Route path="/purchase" element={<PurchasePage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/communication" element={<CommunicationPage />} />
-          {/* <Route path="/ticket" element={<Ticketing />} /> */}
-
         </Routes>
       </Suspense>
     </BrowserRouter>
