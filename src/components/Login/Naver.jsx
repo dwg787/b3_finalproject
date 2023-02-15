0991764eb7e43b841a099fc616f7c01037b79c77
Wonseok <dnjstjr970302@gamil.com>
2023-02-15 15:30:45 +0900

    design/로그인 모달 작업중...

--- src/components/Login/Naver.jsx
+++ src/components/Login/Naver.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const Naver = ({ setGetToken, setUserInfo }) => {
-  const [userName, setUserName] = useState("");
+  const [userName, setUserName] = useState('');
   console.log(userName);
   const { naver } = window;
-  const NAVER_CLIENT_ID = "o47rUj6rR0GWdh1UKf95";
-  const NAVER_CALLBACK_URL = "http://localhost:3000/";
+  const NAVER_CLIENT_ID = 'o47rUj6rR0GWdh1UKf95';
+  const NAVER_CALLBACK_URL = 'http://localhost:3000/';
 
   const initializeNaverLogin = () => {
     const naverLogin = new naver.LoginWithNaverId({
@@ -13,7 +13,7 @@ const Naver = ({ setGetToken, setUserInfo }) => {
       callbackUrl: NAVER_CALLBACK_URL,
       // 팝업창으로 로그인을 진행할 것인지?
       isPopup: true,
-      loginButton: { color: "green", type: 1, height: 30 },
+      loginButton: { color: 'green', type: 1, height: 30 },
       callbackHandle: true,
     });
     naverLogin.init();
@@ -28,11 +28,11 @@ const Naver = ({ setGetToken, setUserInfo }) => {
   };
 
   const userAccessToken = () => {
-    window.location.href.includes("access_token") && getToken();
+    window.location.href.includes('access_token') && getToken();
   };
 
   const getToken = () => {
-    const token = window.location.href.split("=")[1].split("&")[0];
+    const token = window.location.href.split('=')[1].split('&')[0];
 
     // 이후 로컬 스토리지 또는 state에 저장하여 사용하자!
     // sessionStorage.setItem("id", token.user.userid);
