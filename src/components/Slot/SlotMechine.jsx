41f4e3bab306a94a7765fc3b5f7d9e59d6726445
(yooyoungjae) <(dbdudwo0910@gmail.com)>
2023-02-17 22:48:54 +0900

    slot mashine

--- src/components/Slot/SlotMechine.jsx
+++ src/components/Slot/SlotMechine.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import noimg from '../../assets/noimg.avif';
 
 //v1 슬롯머신
 
@@ -7,9 +10,18 @@ const SlotMachine = () => {
   //전체적인 api
   const [totalSpots, setTotalSpots] = useState([]);
 
+  //[장소. 맛집, 숙박] 데이터를 담아주는 State
   const [currentSpots, setCurrentSpots] = useState({});
+  const [currentSpots1, setCurrentSpots1] = useState({});
+  const [currentSpots2, setCurrentSpots2] = useState({});
+  const [currentSpots3, setCurrentSpots3] = useState({});
+  const [currentSpots4, setCurrentSpots4] = useState({});
+  const [currentSpots5, setCurrentSpots5] = useState({});
+  const [currentSpots6, setCurrentSpots6] = useState({});
+
   //   const [totalRestaurants, setTotalRestaurants] = useState([]);
   //   const [totalHotels, setTotalHotels] = useState([]);
+  console.log(currentSpots);
 
   //api가 가져오는 로직
   const fetchSpotSearchData = async () => {
@@ -18,6 +30,7 @@ const SlotMachine = () => {
     );
     return setTotalSpots(res.data.response.body.items.item);
   };
+
   //api 화면랜더링시 실행
   useEffect(() => {
     fetchSpotSearchData();
@@ -30,6 +43,32 @@ const SlotMachine = () => {
     return totalSpots[Math.floor(Math.random() * totalSpots.length)];
   };
 
+  const getRandomSpot1 = () => {
+    return totalSpots[Math.floor(Math.random() * totalSpots.length)];
+  };
+
+  const getRandomSpot2 = () => {
+    return totalSpots[Math.floor(Math.random() * totalSpots.length)];
+  };
+
+  const getRandomSpot3 = () => {
+    return totalSpots[Math.floor(Math.random() * totalSpots.length)];
+  };
+
+  const getRandomSpot4 = () => {
+    return totalSpots[Math.floor(Math.random() * totalSpots.length)];
+  };
+
+  const getRandomSpot5 = () => {
+    return totalSpots[Math.floor(Math.random() * totalSpots.length)];
+  };
+
+  const getRandomSpot6 = () => {
+    return totalSpots[Math.floor(Math.random() * totalSpots.length)];
+  };
+
+  //v2 기능 구현예정 아마도...?
+
   //   const getRandomRestaurants = () => {
   //     return slot[1][Math.floor(Math.random() * slot[1].length)];
   //   };
@@ -39,19 +78,162 @@ const SlotMachine = () => {
   //   };
 
   // const buttonClick = () => {}
+  console.log(currentSpots);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+  };
+
+  //   <div>
+  //   <SlotImg1 src={currentSpots1.firstimage || noimg} alt="" />
+  // </div>
+  // <div>
+  //   <SlotImg2 src={currentSpots2.firstimage || noimg} alt="" />
+  // </div>
+  // <div>
+  //   <SlotImg3 src={currentSpots3.firstimage || noimg} alt="" />
+  // </div>
+  // <div>
+  //   <SlotImg4 src={currentSpots4.firstimage || noimg} alt="" />
+  // </div>
+  // <div>
+  //   <SlotImg5 src={currentSpots5.firstimage || noimg} alt="" />
+  // </div>
+  // <div>
+  //   <SlotImg6 src={currentSpots6.firstimage || noimg} alt="" />
+  // </div>
 
   return (
-    <>
+    <WrapDiv>
+      <ImageBox1>
+        <SlotImg1 src={currentSpots1.firstimage || noimg} alt="" />
+        <SlotImg2 src={currentSpots2.firstimage || noimg} alt="" />
+        <SlotImg3 src={currentSpots3.firstimage || noimg} alt="" />
+        <SlotImg4 src={currentSpots4.firstimage || noimg} alt="" />
+        <SlotImg5 src={currentSpots5.firstimage || noimg} alt="" />
+        <SlotImg6 src={currentSpots6.firstimage || noimg} alt="" />
+      </ImageBox1>
+      {/* <Slider>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider> */}
+
+      {currentSpots.firstimage !== '' ? (
+        <ImageBox>
+          <SlotImg src={currentSpots.firstimage} alt="" />
+        </ImageBox>
+      ) : (
+        <ImageBox>
+          <SlotImg src={noimg} alt="" />
+        </ImageBox>
+      )}
       <div>{currentSpots.title}</div>
       <button
         onClick={() => {
           setCurrentSpots(getRandomSpot());
+          setCurrentSpots1(getRandomSpot1());
+          setCurrentSpots2(getRandomSpot2());
+          setCurrentSpots3(getRandomSpot3());
+          setCurrentSpots4(getRandomSpot4());
+          setCurrentSpots5(getRandomSpot5());
+          setCurrentSpots6(getRandomSpot6());
         }}
       >
-        gdgdgd
+        관광지 랜덤뽑기!
       </button>
-    </>
+    </WrapDiv>
   );
 };
 
 export default SlotMachine;
+
+const WrapDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const SlotImg = styled.img`
+  width: 500px;
+  height: 500px;
+`;
+
+const ImageBox = styled.div`
+  width: 1000px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageBox1 = styled.div`
+  width: 1200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  /* overflow: hidden; */
+`;
+
+const SlotImg1 = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+
+const SlotImg2 = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const SlotImg3 = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const SlotImg4 = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const SlotImg5 = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const SlotImg6 = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+
+const SliderBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+`;
+
+const StyledSlider = styled(Slider)`
+  .slick-prev::before,
+  .slick-next::before {
+    opacity: 0;
+    display: none;
+  }
+`;
