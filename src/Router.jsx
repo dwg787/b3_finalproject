8377e2d29333d193208075b15291a6196156c809
dwg787 <forthevision7@gmail.com>
2023-02-27 10:18:27 +0900

    Router 정리

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,25 +4,19 @@ import Loader from './components/Loader/Loader';
 import Navbar from './components/Navbar';
 import ChatBotModal from './components/bot/ChatBotModal';
 import TopButton from './components/TopButton/TopButton';
-// import Ticketing from './components/Reservation/Ticketing';
+import SlotMachine from './components/Slot/SlotMechine';
 
 import {
   MainPage,
   LoginPage,
-  ReservationPage,
   MyPage,
   DetailPage,
   MapPage,
-  PurchasePage,
   SignUpPage,
   SearchPage,
   StayDetailPage,
   RestaurantDetailPage,
 } from './pages';
-
-import Footer from './components/Footer/Footer';
-import SlotMachine from './components/Slot/SlotMechine';
-import styled from 'styled-components';
 
 const Router = () => {
   return (
@@ -39,14 +33,10 @@ const Router = () => {
           <Route path="/:id/map" element={<MapPage />} />
           <Route path="/stay/:id" element={<StayDetailPage />} />
           <Route path="/restaurant/:id" element={<RestaurantDetailPage />} />
-          {/* <Route path="/stay/:id/reservation" element={<ReservationPage />} /> */}
-          <Route path="/reservation" element={<ReservationPage />} />
-          <Route path="/purchase" element={<PurchasePage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/slot" element={<SlotMachine />} />
         </Routes>
-        {/* <Footer /> */}
       </BrowserRouter>
     </Suspense>
   );
