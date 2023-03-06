b84b29e1cf2f69685e437706d144099da75e6b5d
Wonseok <dnjstjr970302@gamil.com>
2023-03-06 11:40:51 +0900

    fix/모바일시 숙박/식당 댓글창 안보이는 오류 수정

--- src/components/Review/Communication.tsx
+++ src/components/Review/Communication.tsx
@@ -139,7 +139,9 @@ const Communication = () => {
         <ReviewBoxList>
           {reviews
             .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+            //현재 페이지에 보여줄 리뷰 목록을 slice로 추출
             .map((review, i) => {
+              //추출한 리뷰 목록을 돌아서
               if (review.paramId === params.id) {
                 // 상세페이지 params.id와 리뷰의  paramId가 같은것만 보여주기
                 return (
@@ -183,12 +185,12 @@ const ReviewContainerWrap = styled.div`
   box-sizing: border-box;
   box-shadow: 2.25685px 2.25685px 5.64213px rgba(0, 0, 0, 0.18);
   border-radius: 11.2843px;
-  @media screen and (max-width: 820px) {
+  /* @media screen and (max-width: 820px) {
     width: 340px;
     height: 460px;
     box-shadow: 0.795985px 0.795985px 1.98996px rgba(0, 0, 0, 0.18);
     border-radius: 3.97992px;
-  }
+  } */
 `;
 
 const PaginationBox = styled.div`
@@ -233,9 +235,9 @@ const PaginationBox = styled.div`
 const BottomLine = styled.div`
   width: 95%;
   border-bottom: 1.5px solid #9eabff;
-  @media screen and (max-width: 820px) {
+  /* @media screen and (max-width: 820px) {
     width: 100%;
-  }
+  } */
 `;
 
 const ReviewContainer = styled.div`
@@ -244,10 +246,10 @@ const ReviewContainer = styled.div`
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
-  @media screen and (max-width: 820px) {
+  /* @media screen and (max-width: 820px) {
     width: 340px;
     height: 460px;
-  }
+  } */
 `;
 
 const ReviewBox = styled.div`
@@ -263,10 +265,10 @@ const ReviewLabel = styled.label`
   margin-top: 48.52px;
   margin-bottom: 15px;
   margin-left: 50px;
-  @media screen and (max-width: 820px) {
+  /* @media screen and (max-width: 820px) {
     margin-left: 20px;
     font-size: 8px;
-  }
+  } */
 `;
 
 const InputAndBtnWrap = styled.form`
@@ -299,7 +301,7 @@ const ReviewInput = styled.input`
   &:focus {
     outline: 1px solid #6478ff;
   }
-  @media screen and (max-width: 820px) {
+  /* @media screen and (max-width: 820px) {
     box-shadow: 0.795985px 0.795985px 1.98996px rgba(0, 0, 0, 0.15);
     border-radius: 2.78595px;
     width: 304px;
@@ -311,7 +313,7 @@ const ReviewInput = styled.input`
     &::placeholder {
       font-size: 10px;
     }
-  }
+  } */
 `;
 
 const ReviewButton = styled.button`
@@ -326,13 +328,13 @@ const ReviewButton = styled.button`
   font-weight: 500;
   font-size: 12.8451px;
   margin-bottom: 20px;
-  @media screen and (max-width: 820px) {
+  /* @media screen and (max-width: 820px) {
     width: 160px;
     height: 39px;
     box-shadow: 2.1118px 2.1118px 5.27949px rgba(0, 0, 0, 0.18);
     border-radius: 9px;
     font-size: 15px;
-  }
+  } */
 `;
 
 const ReviewBoxList = styled.div`
@@ -345,10 +347,10 @@ const ReviewBoxList = styled.div`
   /* border: 1px solid red; */
   margin-left: 5px;
   overflow: hidden;
-  @media screen and (max-width: 820px) {
+  /* @media screen and (max-width: 820px) {
     align-items: center;
     justify-content: center;
     margin-bottom: 15px;
     margin: 0;
-  }
+  } */
 `;
