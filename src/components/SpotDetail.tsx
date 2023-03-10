71d03a941d22159758b3474996e202fa93f9fca7
dwg787 <forthevision7@gmail.com>
2023-03-10 14:41:29 +0900

    design: 스켈레톤 UI 디자인 수정 및 지역별 버튼 조정

--- src/components/SpotDetail.tsx
+++ src/components/SpotDetail.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState, useCallback } from 'react';
 const SpotDetail = (props: FetchedStayDataType) => {
   const navigate = useNavigate();
   const [likeData, setLikeData] = useState<DocumentData | undefined>();
+
   const spotRecommendationList = useCallback(async () => {
     const fbdata = await getDoc(doc(db, 'spot_recommendation', `${props.id}`));
     if (fbdata) {
